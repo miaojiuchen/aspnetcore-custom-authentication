8a1b8e3c95e6c3b8d6f06ce883b38b59aba27b99
FAREAST\v-jimia <v-jimia@microsoft.com>
2020-08-28 09:52:57 +0800

    add missing file

---
+++ Authentication/AuthenticationConstants.cs
@@ -0,0 +1,12 @@
+namespace Auth.Authentication
+{
+    public static class AuthenticationConstants
+    {
+        public const string AdminPolicyName = "AdminPolicy";
+        public const string OAuthGithubPolicyName = "OAuthGithubPolicy";
+        public const string BasicAuthCookieName = "OAuthGithubCookie";
+        public const string OAuthGithubCookieName = "OAuthGithubCookie";
+        public const string OAuthGithubCookieScheme = "OAuthGithubCookie";
+        public const string OAuthGithubScheme = "OAuthGithub";
+    }
+}
